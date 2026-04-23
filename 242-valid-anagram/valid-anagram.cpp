00114021a1ab2aa3cda2f08145b6808847a0e551
Ashutosh Raj <148970228+Ashutoshraj25@users.noreply.github.com>
2026-04-23 21:34:48 +0530

    Time: 8 ms (7.07%) | Memory: 10.1 MB (10.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,25 +3,19 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
         unordered_map<char,int> mp1;
-        unordered_map<char,int> mp2;
 
         for(int i =0;i<s.length();i++){
             mp1[s[i]]++;
         }
-
-        for(int i =0;i<t.length();i++){
-            mp2[t[i]]++;
-        }
-        for(auto x : mp1){
-            char ch1 = x.first;
-            int freq1 = x.second;
-
-            if(mp2.find(ch1)!=mp2.end()){
-                int freq2 = mp2[ch1];
-                if(freq1!=freq2) return false;
+        for(int i =0;i<t.size();i++){
+            char ch = t[i];
+            if(mp1.find(ch)!=mp1.end()){
+                mp1[ch]--;
+                if(mp1[ch] == 0) mp1.erase(ch);
             }
             else return false;
         }
-        return true;
+        if(mp1.size()>0) return false;
+        else return true;
     }
 };
