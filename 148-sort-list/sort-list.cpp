1d3b229091af871a288fbd82104c64d8b8423448
Ashutosh Raj <148970228+Ashutoshraj25@users.noreply.github.com>
2026-04-28 22:09:14 +0530

    Time: 43 ms (39.48%) | Memory: 75.7 MB (17.39%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        vector<int> ans;
-        ListNode* temp = head;
+    ListNode* merge(ListNode* a , ListNode* b){
+        ListNode* c= new ListNode(200);
+        ListNode* temp = c;
 
-        while(temp!=NULL){
-            ans.push_back(temp->val);
-            temp = temp->next;
+        while(a!=NULL && b!=NULL){
+            if(a->val <= b->val){
+                temp->next = a;
+                a = a->next;
+                temp = temp->next;
+            }
+            else{
+                temp->next = b;
+                b = b->next;
+                temp = temp->next;
+            }
         }
-        sort(ans.begin(),ans.end());
-        int i = 0;
-        temp = head;
-        while(temp!=NULL){
-            temp->val = ans[i];
-            temp = temp->next;
-            i++;
+        if(a==NULL) temp->next = b;
+        else temp->next = a;
+        return c->next;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next ==NULL) return head;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        return head;
+
+        ListNode* a = head;
+        ListNode* b = slow->next;
+
+        slow->next = NULL;
+
+        a = sortList(a);
+        b = sortList(b);
+
+        ListNode* c = merge(a,b);
+        return c;
     }
 };
