4e08ba7459e2ea8910b62aa020fe45e3d053fffa
Ashutosh Raj <148970228+Ashutoshraj25@users.noreply.github.com>
2026-06-01 20:46:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (47.74%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     int level(TreeNode* root){
-        if(root== NULL) return 0;
-        int lst = level(root->left);
-        int rst = level(root->right);
-        return (1+max(lst,rst));
+        if(root == NULL) return 0;
+
+        int lst =level(root->left);
+        int rst =level(root->right);
+        return 1+max(lst,rst);
     }
-    void nthLevel(TreeNode* root , int curr , int lvl , vector<int>& v){
+    void norder(TreeNode* root , int curr , int lvl,vector<int>& v){
         if(root == NULL) return;
 
-        if(curr == lvl){
-            v.push_back(root->val);
-        }
+        if(curr == lvl) v.push_back(root->val);
 
-        if(lvl % 2 == 0){
-            nthLevel(root->right,curr+1,lvl,v);
-            nthLevel(root->left,curr+1,lvl,v);
+        if(lvl%2==0){
+            norder(root->right,curr+1,lvl,v);
+            norder(root->left,curr+1,lvl,v);
         }
         else{
-            nthLevel(root->left,curr+1,lvl,v);
-            nthLevel(root->right,curr+1,lvl,v);
+            norder(root->left,curr+1,lvl,v);
+            norder(root->right,curr+1,lvl,v);
         }
     }
-    void lOrder(TreeNode* root, vector<vector<int>>& ans){
+    void levelorder(TreeNode* root,vector<vector<int>>& ans){
         if(root == NULL) return;
         int n = level(root);
-        for(int i = 1;i<=n;i++){
+        for(int i =1;i<=n;i++){
             vector<int> v;
-            nthLevel(root,1,i,v);
+            norder(root,1,i,v);
             ans.push_back(v);
         }
     }
-
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> ans;
-        lOrder(root,ans);
+        levelorder(root,ans);
         return ans;
     }
 };
