6338fb6a3491eeaeb341f4c32a53e7fa826f507d
Ashutosh Raj <148970228+Ashutoshraj25@users.noreply.github.com>
2026-06-09 19:29:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (66.51%) - LeetSync

---
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxSatisfaction(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        vector<int> suf(n);
+
+        suf[n-1] = arr[n-1];
+        for(int i =n-2;i>=0;i--){
+            suf[i] = suf[i+1]+arr[i];
+        }
+        int idx = -1;
+        for(int i =0;i<n;i++){
+            if(suf[i]>=0){
+                idx = i;
+                break;
+            }
+        }
+        if(idx == -1) return 0;
+        int x= 1;
+        int sum = 0;
+        for(int i = idx;i<n;i++){
+            sum += arr[i]*x;
+            x++;
+        }
+        return sum;
+    }
+};
