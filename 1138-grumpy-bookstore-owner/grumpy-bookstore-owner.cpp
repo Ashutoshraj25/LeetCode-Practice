c18cb304b63317f89da36030520768d70ce9183d
Ashutosh Raj <148970228+Ashutoshraj25@users.noreply.github.com>
2026-06-21 18:45:30 +0530

    Time: 0 ms (100.00%) | Memory: 35.9 MB (42.04%) - LeetSync

---
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& arr, vector<int>& grr, int k) {
+        int prevloss = 0;
+        int n = arr.size();
+
+        for(int i = 0; i < k; i++) {
+            if(grr[i] == 1)
+                prevloss += arr[i];
+        }
+
+        int maxloss = prevloss;
+        int idx = 0;
+
+        int i = 1;
+        int j = k;
+
+        while(j < n) {
+            int curloss = prevloss;
+
+            if(grr[j] == 1)
+                curloss += arr[j];
+
+            if(grr[i - 1] == 1)
+                curloss -= arr[i - 1];
+
+            if(maxloss < curloss) {
+                maxloss = curloss;
+                idx = i;
+            }
+
+            prevloss = curloss;
+            i++;
+            j++;
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(grr[i] == 0)
+                ans += arr[i];
+        }
+
+        ans += maxloss;
+
+        return ans;
+    }
+};
