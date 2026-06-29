a2a75198f2c62499bf5c67f73cb6d294b79ccc69
Ashutosh Raj <148970228+Ashutoshraj25@users.noreply.github.com>
2026-06-29 11:54:30 +0530

    Time: 4 ms (10.70%) | Memory: 74.7 MB (14.48%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
+    int solve(int n,vector<int>& nums, vector<int>& dp){
+        if(n==0) return nums[n];
+
+        if(dp[n]!=-1) return dp[n];
+
+        int take = nums[n]+solve(n-1,nums,dp);
+        int not_take = nums[n];
+
+        return dp[n] = max(take,not_take);
+    }
     int maxSubArray(vector<int>& nums) {
-        int curr = nums[0];
+        int n = nums.size();
+        vector<int> dp(n,-1);
         int ans = nums[0];
-
-        for (int i = 1; i < nums.size(); i++) {
-            curr = max(nums[i], curr + nums[i]);
-            ans = max(ans, curr);
+        for(int i =0;i<n;i++){
+            ans =  max(ans,solve(i,nums,dp));
         }
-
         return ans;
     }
 };
